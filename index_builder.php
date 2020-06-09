badfdd58480de69f419e5fde51c30c5a37a66076
Lukas Clausson <lukas.clausson@hotmail.com>
2020-06-09 15:13:33 +0200

    Build Fixes

--- index_builder.php
+++ index_builder.php
@@ -1,46 +1,31 @@
 <?php
 
 // Read page_header.html
-$header = file_get_contents('content/page_header.html');
+$page_content = file_get_contents('content/page_header.html');
 
 // Read Projects JSON
 $projects_data = file_get_contents('content/projects.json');
 $projects = json_decode($projects_data);
 
-// Read page_footer.html
-$footer = file_get_contents('content/page_footer.html');
-
-
-$proj_01 =
-'<div class="site-section" id="portfolio-section">
-    <div class="container">
-        <div class="row mb-5 ">
-            <div class="col-md-7 section-title text-center mx-auto">
-                <span class="sub-title mb-2 d-block">Portfolio</span>
-                <h2 class="title text-primary mb-3">Latest Games & Projects</h2>
-            </div>
-        </div>'
-;
-
-$proj_02 = "";
-
 $flip = true;
 foreach($projects as $proj) :
 
 
-$proj_02 += '<div class="row mb-5">';
+$page_content .= '<div class="row mb-5">';
 
-$proj_02 += ($flip ? "<div class='col-lg-6 mb-4 mb-lg-0'>" : "<div class='col-lg-6 order-1 order-lg-2 mb-4 mb-lg-0'>");
+$page_content .= ($flip ? "<div class='col-lg-6 mb-4 mb-lg-0'>" : "<div class='col-lg-6 order-1 order-lg-2 mb-4 mb-lg-0'>");
 
-$proj_02 += '<img src="<?php echo $proj->splash; ?>" alt="Image" class="img-fluid">
+$page_content .= '<img src="';
+$page_content .= $proj->splash;
+$page_content .= '" alt="Image" class="img-fluid">
         <p></p>
         <div class="owl-carousel slide-one-item normal-slider">';
 
 foreach($proj->screenshots as $path) {
-    $proj_02 += "<div class='slide'><img src='{$path}' alt='Image' class='img-fluid'></div>";
+    $page_content .= "<div class='slide'><img src='{$path}' alt='Image' class='img-fluid'></div>";
 }
         
-$proj_02 += '</div>
+$page_content .= '</div>
         <table style="margin-top: 40px;" class="table table-striped table-borderless">
           <thead>
             <th>Specifications</th>
@@ -48,60 +33,66 @@ $proj_02 += '</div>
           <tbody>
             <tr>
               <th>Engine</th>
-              <th><?php echo $proj->specs->engine; ?></th>
+              <th>';
+$page_content .= $proj->specs->engine;
+$page_content .= '</th>
             </tr>
             <tr>
               <th>Platform(s)</th>
-              <th><?php echo $proj->specs->platform; ?></th>
+              <th>';
+$page_content .= $proj->specs->platform;
+$page_content .= '</th>
             </tr>
             <tr>
               <th>Input</th>
-              <th><?php echo $proj->specs->input; ?></th>
+              <th>';
+$page_content .= $proj->specs->input;
+$page_content .= '</th>
             </tr>
           </tbody>
         </table>
       </div>';
 
-$proj_02 += ($flip ? "<div class='col-lg-5 h-100 jm-sticky-top ml-auto'>" : "<div class='col-lg-5 h-100 jm-sticky-top mr-auto order-2 order-lg-1'>");
+$page_content .= ($flip ? "<div class='col-lg-5 h-100 jm-sticky-top ml-auto'>" : "<div class='col-lg-5 h-100 jm-sticky-top mr-auto order-2 order-lg-1'>");
         
-$proj_02 += '<h3>';
-$proj_02 += $proj->title;
-$proj_02 += '</h3>
+$page_content .= '<h3>';
+$page_content .= $proj->title;
+$page_content .= '</h3>
         <p class="sub-text mb-4"><strong>';
-$proj_02 += $proj->position;
-$proj_02 += '</strong></p>';
+$page_content .= $proj->position;
+$page_content .= '</strong></p>';
 
 foreach($proj->text as $p)
 {
-    $proj_02 += "<p class='mb-4'>{$p}</p>";
+    $page_content .= "<p class='mb-4'>{$p}</p>";
 }
 
-$proj_02 += '<p><a data-fancybox data-ratio="2" href="';
-$proj_02 += $proj->trailer;
+$page_content .= '<p><a data-fancybox data-ratio="2" href="';
+$page_content .= $proj->trailer;
 
-$proj_02 += '" class="d-flex align-items-center"><span class="icon-play_circle_outline h4 m-0 mr-2"></span> <span>Watch the trailer</span></a></p>
+$page_content .= '" class="d-flex align-items-center"><span class="icon-play_circle_outline h4 m-0 mr-2"></span> <span>Watch the trailer</span></a></p>
         <p class="mb-4"><a href="';
-$proj_02 += $proj->website;
-$proj_02 += '" class="readmore" target="blank">Visit Game Website</a></p>
+$page_content .= $proj->website;
+$page_content .= '" class="readmore" target="blank">Visit Game Website</a></p>
         <div class="testimonial h-100">
           <blockquote class="mb-3">
             <p>&ldquo;';
-$proj_02 += $proj->quote->text;
-$proj_02 += '&rdquo;</p>
+$page_content .= $proj->quote->text;
+$page_content .= '&rdquo;</p>
           </blockquote>
           <div class="d-flex align-items-center vcard">
             <figure class="mb-0 mr-3">
               <img src="';
-$proj_02 += $proj->quote->author_img;
-$proj_02 += '" alt="Image" class="img-fluid ounded-circle">
+$page_content .= $proj->quote->author_img;
+$page_content .= '" alt="Image" class="img-fluid ounded-circle">
             </figure>
             <div class="vcard-text">
               <span class="d-block">';
-$proj_02 += $proj->quote->author;
-$proj_02 += '</span>
+$page_content .= $proj->quote->author;
+$page_content .= '</span>
               <span class="position">';
-$proj_02 += $proj->quote->author_title;
-$proj_02 += '</span>
+$page_content .= $proj->quote->author_title;
+$page_content .= '</span>
             </div>
           </div>
         </div>
@@ -110,9 +101,11 @@ $proj_02 += '</span>
 
     <hr>';
 
+$flip = !$flip;
 endforeach;
 
-$page_content = $header + $proj_01 + $proj_02 + $footer;
+// Read page_footer.html
+$page_content .= file_get_contents('content/page_footer.html');
 
 file_put_contents('index.html', $page_content);
 
