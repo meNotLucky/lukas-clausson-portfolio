6feeeb3e716814713ecfe43d4db54789332bc04a
Lukas Clausson <lukas.clausson@hotmail.com>
2024-06-24 23:08:06 +0200

    Revert "Changed initial projects tab"
    This reverts commit 113602e28058747764bea3f5711bb010b6d4f2e1.

--- public/building/index_builder.php
+++ public/building/index_builder.php
@@ -58,8 +58,8 @@ $page_content .=
     <div class='row' data-aos='fade-up'>
       <div class='col-lg-12 d-flex justify-content-center'>
         <ul id='portfolio-flters'>
-          <li data-filter='*'>All Work</li>
-          <li data-filter='.filter-professional-projects' class='filter-active'>Professional Work</li>
+          <li data-filter='*' class='filter-active'>All Work</li>
+          <li data-filter='.filter-professional-projects'>Professional Work</li>
           <li data-filter='.filter-personal-projects'>Personal Work</li>
           <li data-filter='.filter-student-games'>Student Work</li>
         </ul>
